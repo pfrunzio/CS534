25cf6cb499f0877b165f75f0db65beb3e6e04443
Alex <alexander.siracusa@gmail.com>
2023-02-01 12:09:21 -0500

    command line runs until "q" improved error messages

--- Assignment 1/Main.py
+++ Assignment 1/Main.py
@@ -8,56 +8,65 @@ COMMAND_ARGUMENT_HILLCLIMB = "greedy"
 
 
 def main(argv):
-    if len(argv) <= 0:
+
+    command_format = "\nformat: [npuzzle/greed] [board file] [sliding/teleporting] [true/false]"
+
+    while True:
         argv = get_input()
 
-    if len(argv) < 3:
-        raise Exception("Not enough command line arguments")
+        if len(argv) != 0 and argv[0] == "q":
+            return
 
-    board = None
+        if len(argv) < 3:
+            print("Not enough command line arguments", command_format)
+            continue
 
-    try:
-        directory = './Boards/' + argv[1]
-        board = pd.read_csv(directory, sep=',', header=None).replace('B', 0).values.astype(int)
-    except Exception as e:
-        print(e)
-        return
-
-    algorithm = argv[0]
-
-    driver = None
-
-    if algorithm == COMMAND_ARGUMENT_HILLCLIMB:
+        board = None
 
         try:
-            seconds = int(argv[2])
-        except:
-            print("Exception: Invalid seconds for command argument 3")
-            return
-        driver = HillClimbing.HillClimbing(board, seconds)
+            directory = './Boards/' + argv[1]
+            board = pd.read_csv(directory, sep=',', header=None).replace('B', 0).values.astype(int)
+        except Exception as e:
+            print(e, command_format)
+            continue
 
-    elif algorithm == COMMAND_ARGUMENT_ASTAR:
+        algorithm = argv[0]
 
-        heuristic = argv[2]
-        bool = argv[3].lower()
+        driver = None
 
-        weight = None
+        if algorithm == COMMAND_ARGUMENT_HILLCLIMB:
 
-        if bool == "true":
-            weight = True
-        elif bool == "false":
-            weight = False
+            try:
+                seconds = int(argv[2])
+            except:
+                print("Exception: Invalid seconds for command argument 3", command_format)
+                continue
+            driver = HillClimbing.HillClimbing(board, seconds)
 
-        print(heuristic)
+        elif algorithm == COMMAND_ARGUMENT_ASTAR:
 
-        if not heuristic.lower() in [AStar.HEURISTIC_TELEPORT, AStar.HEURISTIC_SLIDE]:
-            raise Exception("Unknown heuristic for command argument 2")
+            heuristic = argv[2]
+            bool = argv[3].lower()
 
-        driver = AStar.AStar(board, heuristic, weight)
-    else:
-        raise Exception("Unknown algorithm for command argument 1")
+            weight = None
 
-    driver.start()
+            if bool == "true":
+                weight = True
+            elif bool == "false":
+                weight = False
+
+            print(heuristic)
+
+            if not heuristic.lower() in [AStar.HEURISTIC_TELEPORT, AStar.HEURISTIC_SLIDE]:
+                print("Unknown heuristic for command argument 2", command_format)
+                continue
+
+            driver = AStar.AStar(board, heuristic, weight)
+        else:
+            print("Unknown algorithm for command argument 1", command_format)
+            continue
+
+        driver.start()
 
 
 def get_input():
