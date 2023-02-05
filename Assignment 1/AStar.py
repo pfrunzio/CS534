6f62591154c93c9275b5632ad48908b8318e56b1
Paola Frunzio <paolafru24@gmail.com>
2023-02-04 19:50:03 -0500

    from pseudocode

--- Assignment 1/AStar.py
+++ Assignment 1/AStar.py
@@ -1,4 +1,5 @@
 from Algorithm import Algorithm
+from queue import PriorityQueue 
 
 
 class AStar(Algorithm):
@@ -12,4 +13,24 @@ class AStar(Algorithm):
         print(f'Performing A* search with {self.heuristic_type} heuristic {"with" if self.weighted else "without"} weight')
         print("Initial Board:")
         print(self.board)
+    
+        fringe = PriorityQueue()
+        fringe.put(self.board, 0)
+        came_from = dict()
+        cost_so_far = dict()
+        came_from[self.board] = None
+        cost_so_far[self.board] = 0
 
+        while not fringe.empty():
+            current = fringe.get()
+
+            if self.calculate_heuristic(self, current) == 0:
+                break
+            
+            for next in self.neighbors(current):
+                new_cost = cost_so_far[current] + self.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + Algorithm.calculate_heuristic(self, next)
+                    fringe.put(next, priority)
+                    came_from[next] = current
