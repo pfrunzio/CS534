fbeaf71b77fea23b1b0410de5b1733b69b932980
cscopetski <cscopetski@gmail.com>
2023-02-26 19:45:59 -0500

    Part 1 implemented (deterministic)

--- Assignment 2/RL.py
+++ Assignment 2/RL.py
@@ -1,46 +1,129 @@
+import math
+import random
+import time
+
 from Gridworld import Action
+
+EXPLORATION_RATE = 0.2
 
 
 class RL:
     def __init__(self, gridworld, runtime, reward, transition_model, time_based):
         self.gridworld = gridworld
         self.runtime = runtime
-        self.reward = reward
+        self.per_action_reward = reward
         self.transition_model = transition_model
         self.time_based = time_based
-        self.gamma = 1
+        self.future_reward_discount = 1
+        self.step_size_parameter = .9
 
         # (state, action) -> utility
         self.q_table = dict()
 
     def start(self):
-        print(f'Performing RL in {self.runtime} seconds with {self.reward} reward per action and actions succeeding {self.transition_model*100} percent of the time {"taking into account" if self.time_based else "ignoring"} remains.\n')
+        print(
+            f'Performing RL in {self.runtime} seconds with {self.per_action_reward} reward per action and actions succeeding {self.transition_model * 100} percent of the time {"taking into account" if self.time_based else "ignoring"} remains.\n')
         print("Initial World:")
-        print(self.gridworld)
+        print(self.gridworld, '\n')
 
         # start learning
         self._rl()
 
     def _rl(self):
-        # TODO
+        end_time = time.time() + self.runtime
 
-        # How to use the Gridworld code:
-        # new_board, reward, terminal = self.gridworld.take_action(Action.UP, self.reward, self.transition_model)
-        # state = self.gridworld.position
+        count = 0
 
-        # ^ this should be all you need to call from Gridworld
+        while time.time() < end_time:
 
+            terminal = False
+            current_gridworld = self.gridworld
+            current_state = current_gridworld.position
+
+            action = self._select_action(current_state)
+
+            while not terminal:
+                new_board, reward, terminal = current_gridworld.take_action(action, self.per_action_reward, 1)
+                current_gridworld = new_board
+                new_state = current_gridworld.position
+                new_action = self._select_action(new_state)
+
+                self._update_utility(current_state, action, reward, new_state, new_action)
+
+                current_state = new_state
+                action = new_action
+
+
+            # if(count < 11):
+                # count+=1
+                # print('\nPosition: ', current_state, '\n')
+                # print(self.q_table, '\n')
+
+        print(self.q_table, '\n')
         return
 
     def _select_action(self, state):
-        # TODO
-        return
 
-    def _update_utility(self, state, action, reward, new_state):
-        # TODO
+        return self._epsilon_greedy(EXPLORATION_RATE, state)
+
+    def _update_utility(self, state, action, reward, new_state, new_action):
 
         # if you want to do SARSA, add the utility from q_table for a random/epsilon greedy action from new_state
         # if you want to do Q-Learning, add the max utility from q_table for any/all actions from new_state
 
-        return
-        
+        new_utility = self._sarsa_utility(state, action, reward, new_state, new_action, self.step_size_parameter)
+
+        self.q_table[(state, action)] = new_utility
+
+    def _q_learning_utility(self, state, action, reward, new_state, step_size_parameter):
+
+        current_utility = self._get_utility(state, action)
+        new_utility = self._get_utility(new_state, self._get_best_action(new_state))
+
+        return self._calculate_temporal_difference(current_utility, new_utility, reward, step_size_parameter)
+
+    def _sarsa_utility(self, state, action, reward, new_state, new_action, step_size_parameter):
+
+        current_utility = self._get_utility(state, action) #6.264
+        new_utility = self._get_utility(new_state, new_action) #-2.736
+
+        return self._calculate_temporal_difference(current_utility, new_utility, reward, step_size_parameter)
+
+    def _calculate_temporal_difference(self, current_utility, new_utility, reward, step_size_parameter):
+        return current_utility + \
+            (step_size_parameter * (reward + (self.future_reward_discount * new_utility - current_utility))) #-9
+#6.264 + .9 * (6.96 + -9)
+    #4.428
+    def _epsilon_greedy(self, epsilon, state):
+
+        rand = random.random()
+
+        if (rand < epsilon):
+            return self._explore()
+        else:
+            return self._get_best_action(state)
+
+    def _get_best_action(self, state):
+
+        best_utility = -math.inf
+        best_action = Action.UP
+
+        for action in Action:
+
+            current_utility = self._get_utility(state, action)
+
+            if current_utility > best_utility:
+                best_utility = current_utility
+                best_action = action
+
+        return best_action
+
+    def _get_utility(self, state, action):
+        try:
+            return self.q_table[(state, action)]
+        except KeyError as e:
+            # self.q_table[(state, action)] = 0
+            return 0
+
+    def _explore(self):
+        return random.choice(list(Action))
