2be889183df826ca7df4b2c7e6dd0f93885b3102
Edison Zhang <ezhang2@wpi.edu>
2023-03-29 22:33:39 -0400

    Extract Board from CSV

--- Assignment 3/BoardGenerator.py
+++ Assignment 3/BoardGenerator.py
@@ -11,7 +11,7 @@ import BoardGeneratorAStar
 from Board import Board
 
 
-def main(argv):
+def main1(argv):
     command_format = "\ncommands: [Board Size NxN] [Number of Boards]"
 
     print(command_format)
@@ -69,9 +69,23 @@ def generate_board(board_size):
 
     return board_csv
 
+def extract_board_from_file(file):
+    list_of_board = []
+    list_of_board_csv = pd.read_csv(file, sep=',', header=None).values.astype(int)
+    board_size = int((len(list_of_board_csv[0])-1)**(1/2))
+    for x in range(len(list_of_board_csv)):
+        board_csv = list(list_of_board_csv[x])
+        list_of_board.append(create_board_from_csv(board_csv, board_size))
+    return list_of_board
 
 
+def create_board_from_csv(board_csv,board_size):
+    cost = board_csv.pop()
+    board = Board(reformat_board_csv(board_csv, board_size), cost=cost)
+    return board
 
+def reformat_board_csv(board_csv, board_size):
+    return np.reshape(board_csv, (board_size, board_size))
 
 def get_input():
     user_input = input("Enter arguments:")
