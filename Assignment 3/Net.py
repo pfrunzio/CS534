90bc4660550ca4129875a2ec91788557b667758f
Paola Frunzio <paolafru24@gmail.com>
2023-04-05 11:44:53 -0400

    fixing typos

--- Assignment 3/Net.py
+++ Assignment 3/Net.py
@@ -18,13 +18,11 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc4 = nn.Linear(hidden_size, output_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.fc2(x)
         return x
     
 # define the loss function
